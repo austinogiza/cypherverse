5af5bbcf786be376b82b395972af0f7feab9f324
austinogiza <tobi@tangleswap.exchange>
2022-01-18 02:43:14 +0100

    add api

--- src/constants/utils.tsx
+++ src/constants/utils.tsx
@@ -1,6 +1,8 @@
 export const host =
-  process.env.NODE_ENV === "development" ? "http://127.0.0.1:8000" : null;
+  process.env.NODE_ENV === "development"
+    ? "http://127.0.0.1:8000"
+    : "https://api.legendsofcypher.io"
 
-export const baseURL = `http://127.0.0.1:8000`;
+export const baseURL = `http://127.0.0.1:8000`
 
-export const joinWaitList = `${host}/api/join-list/`;
+export const joinWaitList = `${host}/api/join-list/`
